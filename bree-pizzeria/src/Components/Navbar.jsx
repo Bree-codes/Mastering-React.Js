73c41aa104318f4f81d4ba0879005a09a9dbc1ec
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2025-01-27 15:42:15 +0300

    pizzeria website

--- bree-pizzeria/src/Components/Navbar.jsx
+++ bree-pizzeria/src/Components/Navbar.jsx
@@ -1,20 +1,39 @@
 import {Link} from "react-router-dom";
 import "../Styles/Navbar.css"
+import Logo from "../assets/pizzaLogo.png"
+import MenuIcon from "@mui/icons-material/Menu";
+import {useState} from "react";
 
 const Navbar = () => {
+    const [openLinks, setOpenLinks] = useState(false);
+
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks);
+    };
+
     return (
         <div className={"navbar"}>
+           <div className="leftSide" id={openLinks ? "open" : "close"}>
+               <img src={Logo}  alt="logo"/>
+               <div className="hiddenLinks">
+                   <Link to="/"> Home </Link>
+                   <Link to="/menu"> Menu </Link>
+                   <Link to="/about"> About </Link>
+                   <Link to="/contact"> Contact </Link>
+               </div>
+           </div>
 
-
-            <div className={"rightside"}>
+            <div className={"rightSide"}>
                 <Link to ="/"> Home </Link>
                 <Link to ="/menu"> Menu </Link>
                 <Link to ="/about"> About </Link>
                 <Link to ="/contact"> Contact </Link>
 
+                <button onClick={toggleNavbar}>
+                    <MenuIcon/>
+                </button>
+
             </div>
-
-
         </div>
     );
 };
